9bb6ae3b67615e01fdb5108db09ea173508f0639
aswanth6000 <aswanth682@gmail.com>
2024-04-04 22:18:48 +0530

    db connection success

---
+++ backend/src/framework/database/db.conn.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+
+type ConnectionURI = string;
+
+export const dbConnection = async (DB_URL: string) => {
+  let uri: ConnectionURI = DB_URL
+  if (!uri) {
+    console.error('DB_URL not found in environment variables');
+    return;
+  }
+
+  try {
+    mongoose.set('strictQuery', true);
+    await mongoose.connect(uri);
+    console.log('Database connected');
+  } catch (err) {
+    console.error('Connection failed:', err);
+  }
+};
--- backend/src/framework/index.ts
+++ backend/src/framework/index.ts
@@ -1,15 +1,18 @@
 import express, { Express, Request, Response } from "express";
+import { dbConnection } from "./database/db.conn";
 import dotenv from "dotenv";
 
 dotenv.config();
+console.log(process.env);
+
 
 const app: Express = express();
 const port = process.env.PORT || 8080;
+const DB_URL = process.env.DB_URL!
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
+
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+    console.log(`[server]: Server is running at http://localhost:${port}`);
 });
+dbConnection(DB_URL)
